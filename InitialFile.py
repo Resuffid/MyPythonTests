3eda33036dd6b51db98a98576deec3203aa2add4
Wiktor <wiktorw336@gmail.com>
2022-05-26 12:44:29 +0200

    Added unpacking, global

--- InitialFile.py
+++ InitialFile.py
@@ -27,3 +27,32 @@ print(type(a))
 # A variable name cannot start with a number
 # A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 # Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+Variable_I = "My name is"
+print(Variable_I, " Wiktor")
+
+# Many values
+d, e, f = "Orange", "Banana", "Cherry"
+print(d)
+print(e)
+print(f)
+
+# Unpacking
+fruits = ["apple", "banana", "cherry"]
+g, h, i = fruits
+print(g)
+print(h)
+print(i)
+
+# Global variable
+
+o = "awesome"
+
+def myfunc():
+  global o
+  o = "fantastic"
+  print("Python is", o)
+
+myfunc()
+
+print("Python is " + o)
