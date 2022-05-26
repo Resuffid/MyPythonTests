7c058d4c43e4659886e6493b5501acf6ca32937b
Wiktor <wiktorw336@gmail.com>
2022-05-26 11:50:13 +0200

    Initial file

---
+++ InitialFile.py
@@ -0,0 +1,29 @@
+print("Hello, World")
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+x = 5
+y = "Hello world"
+
+print(y, "My age is", x)
+
+# Comment :p
+
+# Types of variables
+
+a = str(3)      # text
+b = int(3)      # integer
+c = float(3)    # real
+
+# To check the type of variable you use command print(type(a))
+
+print(a)
+print(b)
+print(c)
+print(type(a))
+
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
