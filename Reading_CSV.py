58b9e69cdff19fced7709d2d77c325990aa367b4
Wiktor <wiktorw336@gmail.com>
2022-05-27 10:43:12 +0200

    CSV files

---
+++ Reading_CSV.py
@@ -0,0 +1,6 @@
+# Importing of csv file
+import csv
+with open('dane1.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
